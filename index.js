4f056eabb465e7d59b1cbd3960d9717e181ad06d
lvjianqiao <lvjianqiao@tianyancha.com>
2024-03-30 23:14:37 +0800

    add params

--- index.js
+++ index.js
@@ -1,15 +1,82 @@
-// const gitlog = require('gitlog').default;
 const path = require("path");
 const simpleGit = require("simple-git");
 const fg = require("fast-glob");
 const ignoreFile = ["package-lock.json", "yarn.lock"];
+const { Command } = require("commander");
+const program = new Command();
+const moment = require("moment");
+
+program
+  .name("am-I-the-roll-king")
+  .description("我是卷王吗？")
+  .option(
+    "-p, --path <string>",
+    "扫描的文件夹路径，如：/user/projects，默认是当前目录",
+    process.cwd()
+  )
+  .option(
+    "-s, --since <date>",
+    "开始时间(YYYY-MM-DD)，默认是当天",
+    moment().format("YYYY-MM-DD")
+  )
+  .option(
+    "-u, --until <date>",
+    "结束时间(YYYY-MM-DD)，默认是当天",
+    moment().format("YYYY-MM-DD")
+  )
+  .option("-d, --deep <number>", "扫描文件夹深度，默认扫描3层", 3)
+  .option(
+    "-a, --author <string>",
+    "作者，会取git config中的user.name，如果想匹配多个作者，可以用逗号分隔，如zhangesan,lisi"
+  )
+  .option(
+    "-i, --ignoreFile <string>",
+    "忽略统计的文件，会忽略文件名中包含指定字符串的文件，多个字符串用逗号分隔，如yarn.lock,package-lock.json，默认不统计package-lock.json,yarn.lock",
+    "package-lock.json,yarn.lock"
+  )
+  .version("1.0.0", "-v, --version", "版本号")
+  .helpOption("-h, --help", "帮助文档");
+program.addHelpText(
+  "after",
+  `
+示例:
+    $ am-I-the-roll-king //统计当前用户，当前文件夹下，当天的代码提交情况，忽略package-lock.json,yarn.lock文件，扫描文件夹深度是3
+    $ am-I-the-roll-king -s 2024-01-01 -u 2024-12-29 //统计当前用户，当前文件夹下，2024年1月1日到2024年12月29日的代码提交情况，忽略package-lock.json,yarn.lock文件，扫描文件夹深度是3
+    $ am-I-the-roll-king -p "/user/project" //统计当前用户，/user/project文件夹下，2024年1月1日到2024年12月29日的代码提交情况，忽略package-lock.json,yarn.lock文件，扫描文件夹深度是3
+    $ am-I-the-roll-king -d 5 //统计当前用户，当前文件夹下，当天的代码提交情况，忽略package-lock.json,yarn.lock文件，扫描文件夹深度是5
+    $ am-I-the-roll-king -a "zhangsan,lisi" //统计zhangsan,lisi用户，当前文件夹下，当天的代码提交情况，忽略package-lock.json,yarn.lock文件，扫描文件夹深度是3
+    $ am-I-the-roll-king -i "yarn.lock,package-lock.json,aaa.js" //统计当前用户，当前文件夹下，当天的代码提交情况，忽略yarn.lock,package-lock.json,aaa.js文件，扫描文件夹深度是3
+    $ am-I-the-roll-king -s 2024-01-01 -u 2024-12-29 -p "/user/project" -d 5 -a "zhangsan,lisi" -i "yarn.lock,package-lock.json,aaa.js" //统计zhangsan,lisi用户，/user/project文件夹下，2024年1月1日到2024年12月29日的代码提交情况，忽略yarn.lock,package-lock.json,aaa.js文件，扫描文件夹深度是5
+`
+);
+program.parse();
+const options = program.opts();
+const gitShellOptions = {
+  "--numstat": null,
+  "--perl-regexp": null,
+  "--invert-grep": null,
+};
+
 const getEntries = () => {
-  return fg.globSync([path.resolve("/Users/tyc/projects/") + "/**/.git"], {
+  return fg.globSync([path.resolve(options.path) + "/**/.git"], {
     dot: true,
     onlyDirectories: true,
     absolute: true,
     deep: 3,
   });
+};
+const getAuthor = async () => {
+  return await new simpleGit().getConfig("user.name");
+};
+const handleOptions = (author) => {
+  if (options.author) {
+    gitShellOptions["--author"] = `(${options.author.split(",").join("|")})`;
+
+  } else {
+    gitShellOptions["--author"] = `(${author})`;
+  }
+  gitShellOptions["--since"] = options.since;
+  gitShellOptions["--until"] = options.until;
 };
 const handleEntries = async (entries, totalObj) => {
   if (entries.length === 0) {
@@ -22,14 +89,7 @@ const handleEntries = async (entries, totalObj) => {
     const log = await simpleGit({
       baseDir: projectPath,
       binary: "git",
-    }).log({
-      "--author": `(liuxin|lvjianqiao)`,
-      "--numstat": null,
-      "--perl-regexp": null,
-      "--invert-grep": null,
-      "--since": "2024-03-20",
-      "--until": "2024-03-29",
-    });
+    }).log(gitShellOptions);
     if (log.all.length > 0) {
       log.all.forEach((logItem) => {
         if (!logItem) return;
@@ -74,6 +134,8 @@ const handleEntries = async (entries, totalObj) => {
     deletions: 0,
     changes: 0,
   };
+  const { value: author } = await getAuthor();
+  handleOptions(author);
   console.log(`扫描指定文件夹，发现${entries.length}个git仓库`);
   if (entries.length === 0) {
     return;
