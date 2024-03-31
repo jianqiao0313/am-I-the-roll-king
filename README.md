1935d3c2e69346f42212e12e900c6956093f4fba
lvjianqiao <lvjianqiao@tianyancha.com>
2024-03-31 12:13:56 +0800

    1.0.7

--- README.md
+++ README.md
@@ -30,12 +30,13 @@ Usage: aitrk [options]
 我是卷王吗？
 
 Options:
-  -p, --path <string>        扫描的文件夹路径，如：/user/projects，默认是当前目录 
+  -p, --path <string>        扫描的文件夹路径，如："/user/projects"，默认是当前目录 
   -s, --since <date>         开始时间(YYYY-MM-DD)，默认是当天
   -u, --until <date>         结束时间(YYYY-MM-DD)，默认是当天
   -d, --deep <number>        扫描文件夹深度，默认扫描3层
-  -a, --author <string>      作者，会取git config中的user.name，如果想匹配多个作者，可以用逗号分隔，如zhangesan,lisi
-  -i, --ignoreFile <string>  忽略统计的文件，会忽略文件名中包含指定字符串的文件，多个字符串用逗号分隔，如yarn.lock,package-lock.json，默认不统计package-lock.json,yarn.lock
+  -a, --author <string>      作者，会取git config中的user.name，如果想匹配多个作者，可以用逗号分隔，如"zhangesan,lisi"
+  -i, --ignoreFile <string>  忽略统计的文件，会忽略文件名中包含指定字符串的文件，多个字符串用逗号分隔，如"yarn.lock,package-lock.json"，默认不统计package-lock.json,yarn.lock
+  -ip,--ignoreProject <string>  忽略统计的项目名称，多个字符串用逗号分隔，如"project1,project2"，默认不忽略任何项目
   -v, --version              版本号
   -h, --help                 帮助文档
 
@@ -46,6 +47,7 @@ Options:
     $ aitrk -d 5 //统计当前用户，当前文件夹下，当天的代码提交情况，忽略package-lock.json,yarn.lock文件，扫描文件夹深度是5
     $ aitrk -a "zhangsan,lisi" //统计zhangsan,lisi用户，当前文件夹下，当天的代码提交情况，忽略package-lock.json,yarn.lock文件，扫描文件夹深度是3
     $ aitrk -i "yarn.lock,package-lock.json,aaa.js" //统计当前用户，当前文件夹下，当天的代码提交情况，忽略yarn.lock,package-lock.json,aaa.js文件，扫描文件夹深度是3
+    $ aitrk -ip "project1,project2" //统计当前用户，当前文件夹下，当天的代码提交情况，忽略project1,project2项目，忽略package-lock.json,yarn.lock文件，扫描文件夹深度是3
     $ aitrk -s 2024-01-01 -u 2024-12-29 -p "/user/project" -d 5 -a "zhangsan,lisi" -i "yarn.lock,package-lock.json,aaa.js" //统计zhangsan,lisi用户，/user/project文件夹下，2024年1月1日到2024年12月29日的代码提交情况，忽略yarn.lock,package-lock.json,aaa.js文件，扫描文件夹深度是5
 
 ```
--- index.js
+++ index.js
@@ -24,20 +24,32 @@ program
   .description("我是卷王吗？")
   .option(
     "-p, --path <string>",
-    "扫描的文件夹路径，如：/user/projects，默认是当前目录",
+    '扫描的文件夹路径，如："/user/projects"，默认是当前目录',
     process.cwd()
   )
-  .option("-s, --since <date>", "开始时间(YYYY-MM-DD)，默认是当天", moment().subtract(1,'days').format("YYYY-MM-DD"))
-  .option("-u, --until <date>", "结束时间(YYYY-MM-DD)，默认是当天", moment().format("YYYY-MM-DD"))
+  .option(
+    "-s, --since <date>",
+    "开始时间(YYYY-MM-DD)，默认是当天",
+    moment().subtract(1, "days").format("YYYY-MM-DD")
+  )
+  .option(
+    "-u, --until <date>",
+    "结束时间(YYYY-MM-DD)，默认是当天",
+    moment().format("YYYY-MM-DD")
+  )
   .option("-d, --deep <number>", "扫描文件夹深度，默认扫描3层", 3)
   .option(
     "-a, --author <string>",
-    "作者，会取git config中的user.name，如果想匹配多个作者，可以用逗号分隔，如zhangesan,lisi"
+    '作者，会取git config中的user.name，如果想匹配多个作者，可以用逗号分隔，如"zhangesan,lisi"'
   )
   .option(
     "-i, --ignoreFile <string>",
-    "忽略统计的文件，会忽略文件名中包含指定字符串的文件，多个字符串用逗号分隔，如yarn.lock,package-lock.json，默认不统计package-lock.json,yarn.lock",
+    '忽略统计的文件，多个字符串用逗号分隔，如"yarn.lock,package-lock.json"，默认不统计package-lock.json,yarn.lock',
     "package-lock.json,yarn.lock"
+  )
+  .option(
+    "-ip,--ignoreProject <string>",
+    '忽略统计的项目名称，多个字符串用逗号分隔，如"project1,project2"，默认不忽略任何项目'
   )
   .version(packageJson.version, "-v, --version", "版本号")
   .helpOption("-h, --help", "帮助文档");
@@ -51,6 +63,7 @@ program.addHelpText(
     $ aitrk -d 5 //统计当前用户，当前文件夹下，当天的代码提交情况，忽略package-lock.json,yarn.lock文件，扫描文件夹深度是5
     $ aitrk -a "zhangsan,lisi" //统计zhangsan,lisi用户，当前文件夹下，当天的代码提交情况，忽略package-lock.json,yarn.lock文件，扫描文件夹深度是3
     $ aitrk -i "yarn.lock,package-lock.json,aaa.js" //统计当前用户，当前文件夹下，当天的代码提交情况，忽略yarn.lock,package-lock.json,aaa.js文件，扫描文件夹深度是3
+    $ aitrk -ip "project1,project2" //统计当前用户，当前文件夹下，当天的代码提交情况，忽略project1,project2项目，忽略package-lock.json,yarn.lock文件，扫描文件夹深度是3
     $ aitrk -s 2024-01-01 -u 2024-12-29 -p "/user/project" -d 5 -a "zhangsan,lisi" -i "yarn.lock,package-lock.json,aaa.js" //统计zhangsan,lisi用户，/user/project文件夹下，2024年1月1日到2024年12月29日的代码提交情况，忽略yarn.lock,package-lock.json,aaa.js文件，扫描文件夹深度是5
 `
 );
@@ -92,6 +105,17 @@ const handleEntries = async (entries, totalObj) => {
     return;
   }
   const projectPath = path.resolve(entries.shift(), "../");
+  if (options.ignoreProject) {
+    const ignoreProjects = options.ignoreProject.split(",");
+    let ignore = ignoreProjects.some((ignoreProjectItem) =>
+      projectPath.includes(ignoreProjectItem)
+    );
+    if (ignore) {
+      console.log(chalk.bold.underline.cyan(`[忽略处理]${projectPath}项目`));
+      handleEntries(entries, totalObj);
+      return;
+    }
+  }
   console.log(chalk.bold.underline.blue(`[开始处理]${projectPath}项目`));
   try {
     const log = await simpleGit({
@@ -162,11 +186,11 @@ const printResult = (totalObj) => {
   let perDayChange = totalObj.changes / countDays;
   console.log(
     chalk.black.bgYellow.bold(
-      `${options.since}-${options.until}，共${countDays}天，平均insertions: ${
-      (totalObj.insertions / countDays).toFixed(2)
-      }, 平均deletions: ${(totalObj.deletions / countDays).toFixed(2)}, 平均changes: ${
-        (totalObj.changes / countDays).toFixed(2)
-      }`
+      `${options.since}-${options.until}，共${countDays}天，平均insertions: ${(
+        totalObj.insertions / countDays
+      ).toFixed(2)}, 平均deletions: ${(totalObj.deletions / countDays).toFixed(
+        2
+      )}, 平均changes: ${(totalObj.changes / countDays).toFixed(2)}`
     )
   );
   if (perDayChange > 1000) {
